7962876a9bc9429c383423a00e74395efab680d2
Arush Jain <118338628+arushjain609@users.noreply.github.com>
2024-06-19 17:40:03 +0530

    Update Course.jsx

--- frontend/src/components/Course.jsx
+++ frontend/src/components/Course.jsx
@@ -1,6 +1,5 @@
 import {Link } from 'react-router-dom';
 function Course(courses) {
-    
     return (
         <div className="my-courses">
             {
@@ -13,7 +12,7 @@ function Course(courses) {
                             <div className="pie-chart-5">
                             <div className="overlap-group-2">
                                 <div className="ellipse"></div>
-                                <div className="text-wrapper-4">{course.attendance}</div>
+                                <div className="text-wrapper-4-attendance">{course.attendance}</div>
                             </div>
                             </div>
                         </div>
